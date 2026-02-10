e7bc46d39ec6b2928e0e9a4dfea3675c65088376
gofreeil <yahavanter@gmail.com>
2026-02-10 02:38:46 +0200

    כותרת ראשונה בדף

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 
 <main class="premium-container">
     <section class="hero">
-        <h1 class="title-gradient">רכישות קבוציות</h1>
+        <h1 class="title-gradient">הוזלה להוצאות חודשיות</h1>
         <p class="subtitle">
             הצטרפו למהפכת הרכישה המשותפת. יחד אנחנו חזקים יותר, משפיעים יותר
             וחוסכים יותר.
